8a3190bb66ca7f7a507b18fdc74c6d1077383284
Brianne Duffy <duffymbrianne@gmail.com>
2018-10-18 11:47:50 -0600

    completed fetchuser

--- client/src/components/FetchUser.js
+++ client/src/components/FetchUser.js
@@ -1,3 +1,4 @@
+
 import { Component } from 'react'
 import { connect } from 'react-redux'
 import { login } from '../reducers/user'
@@ -12,11 +13,30 @@ class FetchUser extends Component {
       this.loaded()
     } else {
       if (this.checkLocalToken()) {
-        axios.get('/api/auth/valdiate_token')
+        axios.get('/api/auth/validate_token')
+          .then( async res => {
+            await dispatch(login(res.data.data))
+            this.loaded()
+          })
+          .catch( () => this.loaded() )
+      } else {
+        this.loaded()
       }
     }
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (!this.state.loaded) this.loaded()
+  }
+
+  checkLocalToken = () => {
+    const token = localStorage.getItem('access-token')
+    return token
+  }
+
+  loaded = () => {
+    this.setState({ loaded: true })
+  }
 
   render() {
     return this.state.loaded ? this.props.children : null
